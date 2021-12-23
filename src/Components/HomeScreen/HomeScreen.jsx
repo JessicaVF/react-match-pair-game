c0457db50377b22bda14ed892b6f4f2545c3679e
Peter May <peter.may@winzana.com>
2021-12-23 11:22:41 -0600

    add home screen for future use

---
+++ src/Components/HomeScreen/HomeScreen.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import homeScreenStyles from "./HomeScreen.module.css";
+
+const HomeScreen = () => {
+  return (
+    <>
+      <div className={homeScreenStyles.main}>
+        <h1>Hello World</h1>
+      </div>
+    </>
+  );
+};
+
+export default HomeScreen;
--- src/SocialRouter.jsx
+++ src/SocialRouter.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import CardsBoard from "./Components/CardsBoard/CardsBoard";
+import HomeScreen from "./Components/HomeScreen/HomeScreen";
 
 export const SocialRouter = () => {
   return (
     <div className="mainContent">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<CardsBoard />} />
-          <Route path="/home" element={<CardsBoard />} />
+          <Route path="/" element={<HomeScreen />} />
+          <Route path="/home" element={<HomeScreen />} />
           <Route path="/match-pair" element={<CardsBoard />} />
         </Routes>
       </BrowserRouter>
