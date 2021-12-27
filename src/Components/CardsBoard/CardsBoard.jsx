3f37a9d913278a4e892d5293866402de8f96ec2d
JessicaVF <villarjessica@hotmail.com>
2021-12-27 16:05:02 +0100

    commit before merge

--- src/Components/CardsBoard/CardsBoard.jsx
+++ src/Components/CardsBoard/CardsBoard.jsx
@@ -2,27 +2,39 @@ import React, { useEffect, useState } from "react";
 import Cards from "../Cards/Cards";
 import BoardClasses from "./CardsBoard.module.css";
 
-//https://randomuser.me/api/?results=5
+
+
+// We take the photos from an API generator of user data: https://randomuser.me/api/?results=5
 
 const CardsBoard = () => {
   const [imgGroup1, setImgGroup1] = useState(null);
-
+  
   useEffect(() => {
     fetch("https://randomuser.me/api/?results=5")
       .then((res) => res.json())
       .then((data) => {
         setImgGroup1([...data.results]);
+        
       });
   }, []);
 
   //return jsx
   return (
+
+
     <div className={BoardClasses["flxrow"]}>
+
       {/* Game Cards */}
       {imgGroup1 &&
         imgGroup1.map((userObj) => (
           <Cards key={userObj.email} img={userObj.picture.large} />
+          
         ))}
+        {imgGroup1 &&
+        imgGroup1.map((userObj) => (
+          <Cards key={userObj.email} img={userObj.picture.large} />
+        ))}
+        
     </div>
   );
 };
